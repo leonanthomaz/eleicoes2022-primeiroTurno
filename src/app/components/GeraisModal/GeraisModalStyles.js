ae1146dc17acfe39d145ce9d4d647ba5f54ea527
leonanthomaz <leonan.thomaz@gmail.com>
2022-10-22 14:13:10 -0300

    feed gerais

--- src/app/components/GeraisModal/GeraisModalStyles.js
+++ src/app/components/GeraisModal/GeraisModalStyles.js
@@ -1,17 +1,165 @@
 import styled from "styled-components";
 
-export const VT = styled.div`
-    border: 8px solid #3a3a3a; /**/
-    border-left: 8px solid red;
-    /* border-left: ${({branco}) => (branco > (parseFloat(branco)/100) * 25 ? '8px solid purple' : '8px solid red')}; */
-    border-top: ${({validos}) => (parseFloat(validos) > (parseFloat(validos)/100) * 25 ? '8px solid red' : '8px solid gray')};
-    /* border-top: ${({branco}) => (branco > (parseFloat(branco)/100) * 25 ? '8px solid purple' : '8px solid transparent')}; */
-    border-right: ${({validos}) => (parseFloat(validos) > (parseFloat(validos)/100) * 50 ? '8px solid red' : '8px solid gray')};
-    /* border-top: ${({branco}) => (branco > (parseFloat(branco)/100) * 50 ? '8px solid purple' : '8px solid transparent')}; */
-    border-bottom: 8px solid transparent;
-    border-radius: 50%;
-    z-index: 9999999;
+export const Container = styled.div`
 
-    width: 300px;
-    height: 300px;
+`;
+
+export const Box1 = styled.div`
+display: grid;
+grid-template-columns: auto;
+align-items: center;
+justify-content: center;
+`;
+
+export const Box2 = styled.div`
+border: 2px solid red;
+display: grid;
+grid-template-columns: auto auto auto;
+`;
+
+export const Box3 = styled.div`
+border: 2px solid red;
+display: grid;
+grid-template-columns: auto auto;
+align-items: center;
+justify-content: center;
+gap: 20px;
+`;
+
+export const Circulo1 = styled.div`
+border: 2px solid red;
+
+border-radius: 50%;
+width: 220px;
+height: 220px;
+position: relative;
+text-align: center;
+
+@media(max-width: 760px){
+    width: 180px;
+    height: 180px;
+}
+
+.aaa{
+    margin-top: 70px;
+    margin-left: 13px;
+    position: absolute;
+
+    @media(max-width: 760px){
+        margin-top: 65px;
+        margin-left: 16px;
+    }
+    
+    h3{
+    font-size: 34px;
+    font-weight: 700;
+
+    @media(max-width: 760px){
+        font-size: 25px;
+    }
+
+    }
+    span{
+
+        @media(max-width: 760px){
+            font-size: 18px;
+        }
+    }
+}
+
+`;
+
+export const Circulo2 = styled.div`
+border: 2px solid red;
+border-radius: 50%;
+width: 120px;
+height: 120px;
+position: relative;
+text-align: center;
+
+@media(max-width: 760px){
+    width: 100px;
+    height: 100px;
+}
+
+.bbb{
+    margin-top: 25px;
+    margin-left: 10px;
+    position: absolute;
+
+    @media(max-width: 760px){
+        margin-top: 27px;
+        margin-left: 7px;
+    }
+
+    h3{
+    font-size: 18px;
+    font-weight: 700;
+
+    @media(max-width: 760px){
+        font-size: 16px;
+    }
+
+    }
+    span{
+        font-size: 16px;
+
+        @media(max-width: 760px){
+            font-size: 14px;
+        }
+    }
+}
+
+`;
+
+export const Circulo3 = styled.div`
+border: 2px solid red;
+border-radius: 50%;
+width: 120px;
+height: 120px;
+position: relative;
+text-align: center;
+
+@media(max-width: 760px){
+    width: 120px;
+    height: 120px;
+}
+
+.ccc{
+    margin-top: 25px;
+    margin-left: 12px;
+    position: absolute;
+
+    @media(max-width: 760px){
+        margin-top: 25px;
+        margin-left: 12px;
+    }
+
+    h5{
+    font-size: 20px;
+
+    @media(max-width: 760px){
+        font-size: 12px;
+    }
+
+    }
+    span{
+        font-size: 16px;
+        @media(max-width: 760px){
+            font-size: 14px;
+        }
+    }
+}
+
+`;
+
+export const Validos = styled.div`
+border: 2px solid red;
+position: absolute;
+border-radius: 50%;
+
+`;
+
+export const Brancos = styled.div`
+border: 2px solid red;
 `;
--- src/app/components/GeraisModal/index.jsx
+++ src/app/components/GeraisModal/index.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import Modal from 'react-bootstrap/Modal';
-import * as VT from './GeraisModalStyles'
+import * as M from './GeraisModalStyles'
 import { AiOutlineClose } from "react-icons/ai";
 
 export const GeraisModal = ({infoGerais, handleCloseGerais, showGerais }) => {
@@ -26,31 +26,56 @@ export const GeraisModal = ({infoGerais, handleCloseGerais, showGerais }) => {
           }}>Gerais</h1>
           </Modal.Title>
       </Modal.Header>
+
       <Modal.Body style={{background: '#ecececce', padding: '20px'}}>
-      <div className="" style={{display: 'flex', justifyContent: 'center', alignItems: 'center'}}>
-           
-      </div><br/>
-
-      <div className='geral'>
-
-      <VT.VT validos={infoGerais.validos} brancos={infoGerais.branco}/>
-
-      <h1>Validos: {infoGerais.validos}</h1><br/>
-      <h1>Brancos: {infoGerais.branco}</h1><br/>
-      <h1>anulados:{infoGerais.anulados}</h1><br/>
-      <h1>anuladosSJ:{infoGerais.anuladosSJ}</h1><br/>
-      <h1>nulos:{infoGerais.nulos}</h1><br/>
-      <h1>totais:{infoGerais.totais}</h1><br/>
-      </div>
       
-      <div style={{textAlign: 'center', fontSize: '32px', fontWeight: 'bold'}}>
-          {/* <p>{list.numero}</p> */}
-      </div><br/>
-      <h4 style={{textAlign: 'center', fontSize: '16px'}}>
-        {/* {list.partido} */}
-      </h4><br/>
+      <M.Container>
+        <M.Box1>
+        <M.Circulo1>
+              <div className='aaa'>
+                <h3>{Number(infoGerais.totais).toLocaleString('pt-BR')}</h3>
+                <span>Totais</span>
+              </div>
+          </M.Circulo1>
+        </M.Box1>
+
+        <M.Box2>
+          <M.Circulo2>
+              <div className='bbb'>
+                <h3>{Number(infoGerais.validos).toLocaleString('pt-BR')}</h3>
+                <span>Válidos</span>
+              </div>
+          </M.Circulo2>
+            <M.Circulo2>
+                <div className='bbb'>
+                  <h3>{Number(infoGerais.branco).toLocaleString('pt-BR')}</h3>
+                  <span>Brancos</span>
+                </div>
+            </M.Circulo2>
+            <M.Circulo2>
+                <div className='bbb'>
+                  <h3>{Number(infoGerais.nulos).toLocaleString('pt-BR')}</h3>
+                  <span>Nulos</span>
+                </div>
+            </M.Circulo2>
+        </M.Box2>
+
+        <M.Box3>
+          <M.Circulo3>
+              <div className='ccc'>
+                <h5>Anulados</h5>
+                <span>{Number(infoGerais.anulados).toLocaleString('pt-BR')}</span>
+              </div>
+          </M.Circulo3>
+          <M.Circulo3>
+              <div className='ccc'>
+                <h5>Anulados Sob Judice</h5>
+                <span>{Number(infoGerais.anuladosSJ).toLocaleString('pt-BR')}</span>
+              </div>
+          </M.Circulo3>
+        </M.Box3>
+      </M.Container>
       <br/>
-      {/* situação: {list.situacao} */}
       </Modal.Body>
 
       <Modal.Footer>
